85d9d4347ee63a7bdec1c29a5a531e9f933f64d2
suthee suddee <sutsutdi@gmail.com>
2023-04-05 22:57:48 +0700

    step 11 update route with routeObject

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,23 +3,14 @@ import HomePage from "../pages/home-page";
 import RegisterPage from "../pages/register-page";
 import ToastTest from "../pages/toast-test";
 import LoginPage from "../pages/login-page";
-import Dashboard from "../pages/dashboard/d-layout";
-import { Children } from "react";
-import DHome from "../pages/dashboard/d-home";
+import routeDashboard from "./dashboardRoute";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-    children: [
-      // { path: "/", element: <DHome /> },
-      { path: "", element: <DHome /> },
-    ],
-  },
   { path: "/register", element: <RegisterPage /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/toast", element: <ToastTest /> },
+  ...routeDashboard,
 ]);
 
 export default router;
